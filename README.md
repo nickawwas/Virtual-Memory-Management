4c3301ad70ba0051ce41aef28c8a6eeb2890917b
Nicholas Kawwas <nicholas.b.kawwas@gmail.com>
2021-03-23 17:53:55 -0700

    Added Last Accessed to Page

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Simulate Virtual Memory Management and Concurrency Control on a Multicore Comput
 ## Documentation
 - Uses Limited Main Memory and Unlimited Virtual Disk Space
 - Divided into Pages, Storing Variables Consisting of Id and Value\
-  \
+   \
 - Virtual Memory Manager (VMM) Offers 3 Commands
 - Store - Adds Variable to a Page in Memory or Disk If Main Memory is Full
 - Release - Removes then Returns Value from Memory or Disk Given a Variable Id
--- src/main/java/Page.java
+++ src/main/java/Page.java
@@ -2,17 +2,21 @@ public class Page {
     //Stores Variables - (id, value) Pairs
     private String id;
     private int value;
+    //Time Stamp of Last Access
+    private int lastAccessed;
 
     //Default Constructor
     public Page() {
         id = "";
         value = -1;
+        lastAccessed = -1;
     }
 
     //Parameterized Constructor
     public Page(String varName, int varValue) {
         id = varName;
         value = varValue;
+        lastAccessed = -1;
     }
 
     //Set Attributes - Id, Value
@@ -22,6 +26,10 @@ public class Page {
 
     public void setValue(int varValue) {
         value = varValue;
+    }
+
+    public void setLastAccessed(int last) {
+        lastAccessed = last;
     }
 
     //Get Attributes - Id, Value
